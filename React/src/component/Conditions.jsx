536e3c8d45ed5cdfc47104924c75fe7ca703d0ac
pong <156582011+PONGSAPAK-BOONSONTHI@users.noreply.github.com>
2024-03-30 07:04:27 +0700

    UwU

--- React/src/component/Conditions.jsx
+++ React/src/component/Conditions.jsx
@@ -10,17 +10,17 @@ const Conditions = () => {
 
         <div className=' grid w-auto leading-6 my-[30px] gap-6 768:grid-col-2 lg:grid-col-4'>
           {conditions.map((conditions, id) => (
-            <div key={id}>
-              <div>
-                <img src={conditions.gif} alt="gif" />
-                <span>{conditions.title}</span>
+            <div key={conditions} className='h-[370px] p-6 border-[2px] border-[#505050]/[0.314] rounded-2xl bg-[#1a408d40] duration-200'>
+              <div className='flex flex-col items-center justify-between'>
+                <img className='w-auto max-w-full text-center rounded-2xl' src={conditions.gif} alt="gif" />
+                <span className='mt-6 text-center text-sm text-[#edeef0] textN'>{conditions.title}</span>
               </div>
             </div>
           ))}
         </div>
 
-        <div>
-          <span>
+        <div className='text-center'>
+          <span className=' text-sm leading-[22.8px] font-normal text-[#edeef0] textN'>
             คุณสมบัติเพิ่มเติม : ม.ปลาย ศึกษาเกี่ยวกับสายวิทยาศาสตร์ คณิตศาสตร์ และ คอมพิวเตอร์ (หรือสายอื่น ๆ ที่ เกี่ยวข้องกับ คอมพิวเตอร์และเทคโนโลยี)
             <br />
             หรือ ปวช. ในสาขาคอมพิวเตอร์ธุรกิจ, สาขาช่างไฟฟ้ากำลัง (อิเล็กทรอนิกส์), สาขาเมคคาทรอนิกส์ และหุ่นยนต์ (หรือสายอื่น ๆ ที่เกี่ยวข้องกับคอมพิวเตอร์และเทคโนโลยี)
